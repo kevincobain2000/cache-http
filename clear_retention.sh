3d69f8a90cee86ba09b969dee84c84019df49b70
Pulkit Kathuria <kevincobain2000@gmail.com>
2021-03-07 15:12:41 +0900

    README updated

--- clear_retention.sh
+++ clear_retention.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 export $(egrep -v '^#' .env | xargs)
-
+# * * * * * cd /path/to.dir/&& ./clear_retention.sh
 echo "clearing old files"
-find $APP_DIR/assets* -mtime +7 -exec rm {} \;
+find $APP_DIR/assets/*.tar.gz -mtime +7 -exec rm -rf {} \;
